5e235a3a0a68ad280a9766363d5fc4d83412921e
Mahammad <mahammad.rustamov.2001@gmail.com>
2023-09-28 12:08:55 +0400

    Profile section finished

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,7 @@
 "use client";
-import { useState, useEffect } from "react";
+
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
@@ -8,7 +9,19 @@ import Profile from "@components/Profile";
 const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
-  const [posts, setPosts] = useState([]);
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
@@ -21,33 +34,24 @@ const MyProfile = () => {
 
     if (hasConfirmed) {
       try {
-        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
 
-        const filteredPosts = posts.filter((p) => p._id !== post._id);
-        setPosts(filteredPosts);
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+
+        setMyPosts(filteredPosts);
       } catch (error) {
         console.log(error);
       }
     }
   };
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
-      const data = await response.json();
-      setPosts(data);
-    };
-
-    if (session?.user.id) {
-      fetchPosts();
-    }
-  }, []);
-
   return (
     <Profile
       name="My"
-      desc="Welcome to your personalized profile page"
-      data={posts}
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
